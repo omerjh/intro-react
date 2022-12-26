10f0ce6a5e478c528ab66a0b67f7b0e43aaa5b7d
Omer <omerjh@gmail.com>
2022-12-26 09:43:42 -0500

    Updates

---
+++ precache-manifest.46e433019ec9cf0390ee6b4e56a9b759.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8102296f4f50b74055c0",
+    "url": "/intro-react/static/css/main.e48b1051.chunk.css"
+  },
+  {
+    "revision": "8102296f4f50b74055c0",
+    "url": "/intro-react/static/js/main.8102296f.chunk.js"
+  },
+  {
+    "revision": "c3345876b0afb611d661",
+    "url": "/intro-react/static/js/1.c3345876.chunk.js"
+  },
+  {
+    "revision": "f949c68cdf70264507ba",
+    "url": "/intro-react/static/js/runtime~main.f949c68c.js"
+  },
+  {
+    "revision": "5ae767cff34ac56b243b1b7dcd82eeb1",
+    "url": "/intro-react/index.html"
+  }
+];
